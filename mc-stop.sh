127e797e46ffe146298f581413856602beec3144
Mark Ide <cranstonide@gmail.com>
2012-07-30 14:08:39 -0400

    Added the matching stop script which goes with ./mc-start.sh

---
+++ mc-stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Minecraft Stop Script
+# Author: Mark Ide <cranstonide@gmail.com> 
+# Github: https://github.com/cranstonide/linux-minecraft-scripts
+
+# Alert the players that the server is going to be shut down.
+screen -p 0 -S minecraft -X eval "stuff \"say Server will be shutting down in 10 seconds.\"\015"
+
+# Wait a moment and stop the server to do the backup.
+sleep 10
+screen -p 0 -S minecraft -X eval "stuff \"stop\"\015"
