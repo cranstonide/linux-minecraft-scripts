c006baecceaf40e4cedba35c823758f827c0229f
Mark Ide <cranstonide@gmail.com>
2012-12-04 17:42:42 -0500

    added config file

--- mc-stop.sh
+++ mc-stop.sh
@@ -4,6 +4,12 @@
 # Author: Mark Ide <cranstonide@gmail.com> 
 # Github: https://github.com/cranstonide/linux-minecraft-scripts
 
+# Move into the directory with all Linux-Minecraft-Scripts
+cd "$( dirname $0 )"
+
+# Read configuration file
+source mc-config.cfg
+
 # Alert the players that the server is going to be shut down. We stuff the `stop` command into
 # the screen session. \015 is the escape value for a return.
 screen -p 0 -S minecraft -X eval "stuff \"say Server will be shutting down in 10 seconds.\"\015"
