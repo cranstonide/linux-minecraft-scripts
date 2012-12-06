eb9e554973bb891f613d6e23c5a6767f3545cc36
Mark Ide <cranstonide@gmail.com>
2012-12-05 22:56:50 -0500

    Added a restart script, as per @Goten87 suggestion

---
+++ mc-restart.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+# Minecraft Restart Script
+# Author: Mark Ide <cranstonide@gmail.com> 
+# Github: https://github.com/cranstonide/linux-minecraft-scripts
+
+# Move into the directory with all Linux-Minecraft-Scripts
+cd "$( dirname $0 )"
+
+# Read configuration file
+source mc-config.cfg
+
+# Stop the server
+source mc-stop.sh
+
+# Start the server
+source mc-start.sh
