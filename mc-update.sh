d3490076f6169b32466486d7a294eb652e3b5415
Mark Ide <cranstonide@gmail.com>
2012-12-07 00:23:18 -0500

    Added update script (updates minecraft)

---
+++ mc-update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Minecraft Update Script
+# Author: Mark Ide <cranstonide@gmail.com>
+# Github: https://github.com/cranstonide/linux-minecraft-scripts
+
+# Move into the directory with all Linux-Minecraft-Scripts
+cd "$( dirname $0 )"
+
+# Read configuration file
+source mc-config.cfg
+
+# Backup the server
+source mc-backup.sh
+
+# Stop the server
+source mc-stop.sh
+
+# Delete the old jar file, download the new one.
+rm -vf $minecraftDir/$minecraftJar
+wget --output-document=$minecraftDir/$minecraftJar $updateUrl
+
+# Start the server
+source mc-start.sh
